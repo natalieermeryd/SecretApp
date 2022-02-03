5ac25c7b607aa00306ccbac2db8ed0b35cdaa742
nataliaermeryd <natalia.ermeryd@outlook.com>
2022-02-03 22:43:29 +0000

    hash n salt

--- app.js
+++ app.js
@@ -3,8 +3,10 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const ejs = require('ejs');
 const mongoose = require('mongoose');
-//const encrypt = require('mongoose-encryption'); -Raderar dessa för att istället använda HASHING
-const md5 = require('md5'); //HASHING Password - Impossible to reverse
+//!!const encrypt = require('mongoose-encryption'); -Raderar dessa för att istället använda HASHING
+//!!const md5 = require('md5'); UTRADERAD//HASHING Password - Impossible to reverse
+const bcrypt = require('bcrypt');
+const saltRounds = 10;
 // const session = require('express-session');
 // const passport = require('passport');
 // const passportLocalMongoose = require('passport-local-mongoose');
@@ -54,6 +56,7 @@ app.use(bodyParser.urlencoded({
 //if user registers with Google we can only see the Google ID and submitted Secret
 //if user registers via the form, we can see username, encrypted password and secret   
 mongoose.connect('mongodb://localhost:27017/userDB', {useNewUrlParser: true}); //Skapar connection till MongoDB med namnet userDB -Se i Compass
+//mongoose.set("useCreateIndex", true); this is no longer needed in Mongoose 6
 
 const userSchema = new mongoose.Schema ({
     email: String,
@@ -73,8 +76,7 @@ const userSchema = new mongoose.Schema ({
 
 const User = new mongoose.model('User', userSchema); //Skapar User för userSchema
 
-
-//PASSPORT
+//PASSPORT------------------------------------
 
 // passport.use(User.createStrategy());
 
@@ -105,14 +107,16 @@ const User = new mongoose.model('User', userSchema); //Skapar User för userSche
 //   }
 // ));
 
-/* 
+
+/* ----------------------------------------
 ### ALL ROUTES ###
 */ 
+//**HOME ROUTE 
 app.get('/', function(req, res){
     res.render('home') //Skickar användare till home som standard, utgår ifrån Home
 });
 
-//PASSPORT AND GOOGLE
+//**PASSPORT AND GOOGLE ROUTE-------------------
 //opens upp window for using google-sign in
 // app.get('/auth/google',
 //     passport.authenticate('google', { scope: ['profile'] })
@@ -126,7 +130,7 @@ app.get('/', function(req, res){
 //   });
 
 
-//LOGIN ROUTE ----------------------
+//**LOGIN ROUTE ----------------------
 app.get('/login', function(req, res){
     res.render('login') //Skickar användaren till Login
 });
@@ -138,18 +142,92 @@ app.get('/register', function(req, res){
     res.render('register') //Skickar användaren till Register
 });
 
+//**SECRETS ROUTES ----------------------------
+// app.get('/secrets', function (req, res){
+//     //renders all submitted secrets 
+//     User.find({'secret': {$ne: null}}, function(err, foundUsers){
+//         if(err){
+//             console.log(err)
+//         } else {
+//             if(foundUsers) {
+//                 res.render('secrets', {usersWithSecrets: foundUsers});
+//             }
+//         }
+//     });
+// });
+
+//-------------------------------------------
+//if a user wants to access the /submit-page they need to be logged in 
+//- if not, they will be redirected to Log In
+// app.get('/submit', function (req,res) {
+//     if(req.isAuthenticated()){
+//         res.render('submit')
+//     }else {
+//         res.redirect('/login');
+//     }
+// });
+
+//**SUBMIT SECRET ROUTE-------------------------------
+
+// app.post('/submit', function(req, res) {
+//     const submittedSecret = req.body.secret;
+
+    //  Once the user is authenticated and their session gets saved, 
+    //  their user details are saved to req.user.
+    //  console.log(req.user.id);
+
+//     User.findById(req.user.id, function(err, foundUser){
+//         if (err) {
+//             console.log(err)
+//         } else {
+//             if(foundUser) {
+//                 foundUser.secret = submittedSecret;
+//                 foundUser.save(function(){
+//                     res.redirect('/secrets');
+//                 });
+//             }
+//         }
+//     });
+// });
+
+//-----------------------------------------------------
+
+//**LOGOUT ROUTE------------------------------------------
+// app.get('/logout', function(req, res, next) {
+//     // remove the req.user property and clear the login session
+//     req.logout();
+  
+//     // destroy session data
+//     req.session = null;
+  
+//     // redirect to homepage
+//     res.redirect('/');
+//   });
+
+/*
+The user gets redirected to the Secrets-section if registration or log in = success.
+If not, they remain on the same page
+*/
+
+//**REGISTER ROUTE NEW USER */
+
 app.post('/register', function(req, res){
+
+    bcrypt.hash(req.body.password, saltRounds, function(err, hash) {
+    //Store hash in your password DB
+
     const newUser = new User({
         email: req.body.username, //Hämtar Username ifrån register-sidan
-        password: md5(req.body.password) //Hämtar Password ifrån register-sidan -md5 = hashing
-    }); 
-
-    newUser.save(function(err){
-        if (err) {
-            console.log(err);
-        } else {
-            res.render('secrets') //Skickar användaren till sidan Secrets(Loggar in)
-        }
+        password: hash
+        //UTRADERAD MD5 --password: md5(req.body.password) //Hämtar Password ifrån register-sidan -md5 = hashing
+        }); 
+        newUser.save(function(err){
+            if (err) {
+                console.log(err);
+            } else {
+                res.render('secrets') //Skickar användaren till sidan Secrets(Loggar in)
+            }
+         });
     });
 });
 
@@ -182,4 +260,12 @@ app.post('/login', function(req, res){
 
 app.listen(3000, function() {
     console.log("Server running on port 3000 BIATCH");
-}); 
+});
+
+// http.createServer(app).listen(PORT, function(){
+//     console.log('info', `STARTED LISTENING ON PORT ${PORT} BIATCH!`);
+//   });
+  
+//   https.createServer(options, app).listen(443, function(){
+//     console.log('HTTPS listening on 443');
+//   });
