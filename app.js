2da737d27c03c2e973ea7dd29857eb9ea4386936
nataliaermeryd <natalia.ermeryd@outlook.com>
2022-02-04 21:28:12 +0000

    Cookies and Sessions completed with auditlog

--- app.js
+++ app.js
@@ -5,17 +5,18 @@ const ejs = require('ejs');
 const mongoose = require('mongoose');
 //!!const encrypt = require('mongoose-encryption'); -Raderar dessa för att istället använda HASHING
 //!!const md5 = require('md5'); UTRADERAD//HASHING Password - Impossible to reverse
-const bcrypt = require('bcrypt');
-const saltRounds = 10;
-// const session = require('express-session');
-// const passport = require('passport');
-// const passportLocalMongoose = require('passport-local-mongoose');
+//!!const bcrypt = require('bcrypt'); UTRADERAD BCRYPT
+//!!const saltRounds = 10; UTRADERAD BCRYPT SALTING
+const session = require('express-session'); //Cookies and Sessions
+const passport = require('passport');       //Cookies and Sessions  
+const passportLocalMongoose = require('passport-local-mongoose'); //Cookies and Sessions
 // const GoogleStrategy = require('passport-google-oauth20').Strategy;
 // const findOrCreate = require('mongoose-findorcreate');
 
 const MongoClient = require('mongodb').MongoClient;
 
-const auditLog = require('audit-log');
+const auditLog = require('audit-log'); // DECLARE AUDITLOG
+
 const app = express();
 //----SSL-----------------------
 const https = require('https');
@@ -32,13 +33,13 @@ const PORT = process.env.PORT || 3000
 const uri = process.env.MONGODB;
 
 
-//SSL---------------------------------
+//SSL -hemliga nycklar -------------------------
 const options = {
     key: fs.readFileSync('nattas-key.pem'),
     cert: fs.readFileSync('nattas-cert.pem')
 }; //----------------------------------
 
-
+//Övervakar app mot attacker
 app.use('/healthcheck', require('./routes/healthcheck.routes'));
 
 app.use(express.static('public')); //use the location for our css
@@ -47,22 +48,22 @@ app.use(bodyParser.urlencoded({
     extended: true
 }));
 
-// //session cookies
-// app.use(session({
-//     secret:"Our little secret.",
-//     resave: false,
-//     saveUninitialized: true,
-//     cookie: { secure: true }
-// }));
+//session cookies
+app.use(session({
+    secret:"Our little secret.",
+    resave: false,
+    saveUninitialized: false,
+    // cookie: { secure: true }
+}));
 
-// app.use(passport.initialize()); //starting passport encryption
-// app.use(passport.session());    //passport starting session cookies
+app.use(passport.initialize()); //starting passport encryption
+app.use(passport.session());    //passport starting session cookies
 
 //connection to our Mono DB where we have a document for our users.
 //if user registers with Google we can only see the Google ID and submitted Secret
 //if user registers via the form, we can see username, encrypted password and secret   
 mongoose.connect('mongodb://localhost:27017/userDB', {useNewUrlParser: true}); //Skapar connection till MongoDB med namnet userDB -Se i Compass
-//mongoose.set("useCreateIndex", true); this is no longer needed in Mongoose 6
+//mongoose.set("useCreateIndex", true); //this is no longer needed in Mongoose 6
 
 const userSchema = new mongoose.Schema ({
     email: String,
@@ -71,7 +72,7 @@ const userSchema = new mongoose.Schema ({
     //secret: String
 }); //Skapar en ny userSchema i mongoDB och Sparar email & password i mongoDB 
 
-// userSchema.plugin(passportLocalMongoose); // call plugin save = crypting
+userSchema.plugin(passportLocalMongoose); // call plugin save = crypting
 // userSchema.plugin(findOrCreate);          // call plugin find = decrypting
 
 //UTKOMMENTERAT FÖR ATT ISTÄLLET ANVÄNDA HASHING SOM ÄR SÄKRARE KRYPTERING
@@ -82,20 +83,19 @@ const userSchema = new mongoose.Schema ({
 
 const User = new mongoose.model('User', userSchema); //Skapar User för userSchema
 
-//PASSPORT------------------------------------
+// PASSPORT------------------------------------
+    passport.use(User.createStrategy());
 
-// passport.use(User.createStrategy());
-
-// // skapar cookies with user ID
-// passport.serializeUser(function(user, done){
-//     done(null, user.id)
-// });
-// // "opens" the cookie to identify user by ID
-// passport.deserializeUser(function (id, done) {
-//     User.findById(id, function(err, user){
-//         done(err, user);
-//     });
-// });
+// skapar cookies with user ID
+passport.serializeUser(function(user, done){
+    done(null, user.id)
+});
+// "opens" the cookie to identify user by ID
+passport.deserializeUser(function (id, done) {
+    User.findById(id, function(err, user){
+        done(err, user);
+    });
+});
 
 // passport.use(new GoogleStrategy({
 //     clientID: process.env.CLIENT_ID,
@@ -149,6 +149,15 @@ app.get('/register', function(req, res){
 });
 
 //**SECRETS ROUTES ----------------------------
+app.get('/secrets', function (req, res){
+    if (req.isAuthenticated()){
+        res.render('secrets');
+    } else {
+        res.redirect('/login');
+    }
+});
+
+//----ANOTHER SECRET ROUTE-------------------------
 // app.get('/secrets', function (req, res){
 //     //renders all submitted secrets 
 //     User.find({'secret': {$ne: null}}, function(err, foundUsers){
@@ -161,6 +170,8 @@ app.get('/register', function(req, res){
 //         }
 //     });
 // });
+//---------------------------------------------------
+
 
 //-------------------------------------------
 //if a user wants to access the /submit-page they need to be logged in 
@@ -199,16 +210,16 @@ app.get('/register', function(req, res){
 //-----------------------------------------------------
 
 //**LOGOUT ROUTE------------------------------------------
-// app.get('/logout', function(req, res, next) {
-//     // remove the req.user property and clear the login session
-//     req.logout();
+app.get('/logout', function(req, res, next) {
+    // remove the req.user property and clear the login session
+    req.logout();
   
-//     // destroy session data
-//     req.session = null;
+    // // don't destroy session data when logout is used 
+    // req.session = null;
   
-//     // redirect to homepage
-//     res.redirect('/');
-//   });
+    // redirect to homepage
+    res.redirect('/');
+  });
 
 /*
 The user gets redirected to the Secrets-section if registration or log in = success.
@@ -218,55 +229,99 @@ If not, they remain on the same page
 //**REGISTER ROUTE NEW USER */
 
 app.post('/register', function(req, res){
-
-    bcrypt.hash(req.body.password, saltRounds, function(err, hash) {
-    //Store hash in your password DB
-
-    const newUser = new User({
-        email: req.body.username, //Hämtar Username ifrån register-sidan
-        password: hash
-        //UTRADERAD MD5 --password: md5(req.body.password) //Hämtar Password ifrån register-sidan -md5 = hashing
-        }); 
-        newUser.save(function(err){
-            if (err) {
-                console.log(err);
-            } else {
-                res.render('secrets') //Skickar användaren till sidan Secrets(Loggar in)
-            }
-         });
-    });
-});
-
-//LOGIN ROUTES ----------------------
-app.post('/login', function(req, res){
-    const username = req.body.username;
-    const password = req.body.password
-
-    //UTRADERAT MD5 ----const password = md5(req.body.password); //Lägger md5 här också för att kunna matcha den hashade lösenordet.  
-    //Letar i userDB efter inmatad liknelse, om den hittar en identisk sparad ObjectID så kommer den att matcha och logga in
-    User.findOne({email: username}, function(err, foundUser){
-        if (err) {
-            // Error ifall Username och Password inte matchar som i userDB/Compass
+  
+    User.register({username: req.body.username}, req.body.password, function(err, user){
+        if(err){
             console.log(err);
+            res.redirect('/register');
         } else {
-            if (foundUser) {
-                bcrypt.compare(password, foundUser.password, function(err, result) {
-               // res == true
-               if (result === true) {
-                   res.render('secrets');
-                   //Ifall username och password är korrekt så skickas användaren vidare till Secrets-sidan, vilket innebär att användaren blivit autentiserade
-               }
-                });
-
-                //Ifall password matchar med username i userDB så kommer den att logga in, i annat fall blir det error
-                //UTRADERAT MD5 ---if (foundUser.password === password) {
-            }
+            passport.authenticate('local')(req, res, function(){
+                res.redirect('/secrets');
+            });
         }
     });
 });
 
+//------OLD REGISTER ROUTE FOR BCRYPT PASSWORD AND SALTING------
+// app.post('/register', function(req, res){
+
+//     bcrypt.hash(req.body.password, saltRounds, function(err, hash) {
+//     //Store hash in your password DB
+
+//     const newUser = new User({
+//         email: req.body.username, //Hämtar Username ifrån register-sidan
+//         password: hash
+//         //UTRADERAD MD5 --password: md5(req.body.password) //Hämtar Password ifrån register-sidan -md5 = hashing
+//         }); 
+//         newUser.save(function(err){
+//             if (err) {
+//                 console.log(err);
+//             } else {
+//                 res.render('secrets') //Skickar användaren till sidan Secrets(Loggar in)
+//             }
+//          });
+//     });
+// });
+//----------------------------------------------------------------
+
+//LOGIN ROUTES ----------------------
+app.post('/login', function(req, res){
+    const user = new User({
+        username: req.body.username,
+        password: req.body.password
+    });
+    auditLog.logEvent(user.username, 'maybe script name or function',
+    "what just happened", 'the affected target name perhaps', 'target id', 'additional info, JSON, etc.');
+    
+    //This req comes from passport
+    req.login(user, function(err){
+        if(err){
+            console.log(err);
+            res.redirect('/login');
+        } else{
+            passport.authenticate('local')(req, res, function(){
+                res.redirect('/secrets');
+            });
+        }
+    });
+});
+
+//-----OLD LOGIN ROUTE FOR BCRYPT & SALTING-----------
+// app.post('/login', function(req, res){
+//     const username = req.body.username;
+//     const password = req.body.password
+
+//     //UTRADERAT MD5 ----const password = md5(req.body.password); //Lägger md5 här också för att kunna matcha den hashade lösenordet.  
+//     //Letar i userDB efter inmatad liknelse, om den hittar en identisk sparad ObjectID så kommer den att matcha och logga in
+//     User.findOne({email: username}, function(err, foundUser){
+//         if (err) {
+//             // Error ifall Username och Password inte matchar som i userDB/Compass
+//             console.log(err);
+//         } else {
+//             if (foundUser) {
+//                 bcrypt.compare(password, foundUser.password, function(err, result) {
+//                // res == true
+//                if (result === true) {
+//                    res.render('secrets');
+//                    //Ifall username och password är korrekt så skickas användaren vidare till Secrets-sidan, vilket innebär att användaren blivit autentiserade
+//                }
+//                 });
+
+//                 //Ifall password matchar med username i userDB så kommer den att logga in, i annat fall blir det error
+//                 //UTRADERAT MD5 ---if (foundUser.password === password) {
+//             }
+//         }
+//     });
+// });
+//-------------------------------------------------------
 
 
+// friendly recaptcha
+function recaptcha_callback() {
+    var loginBtn = document.querySelector('#login-btn');
+    loginBtn.removeAttribute('disabled');
+    loginBtn.style.cursor = 'pointer';
+}
 
 
 // app.listen(3000, function() {
