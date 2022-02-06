1dff03e6d1a0a5eb9b81455ef2c1ed063cc1ab4c
nataliaermeryd <natalia.ermeryd@outlook.com>
2022-02-06 23:52:28 +0000

    Passport for google login done

--- app.js
+++ app.js
@@ -10,8 +10,8 @@ const mongoose = require('mongoose');
 const session = require('express-session'); //Cookies and Sessions
 const passport = require('passport');       //Cookies and Sessions  
 const passportLocalMongoose = require('passport-local-mongoose'); //Cookies and Sessions
-// const GoogleStrategy = require('passport-google-oauth20').Strategy;
-// const findOrCreate = require('mongoose-findorcreate');
+const GoogleStrategy = require('passport-google-oauth20').Strategy;//Using the const as a strategy
+const findOrCreate = require('mongoose-findorcreate');
 const rateLimit = require('express-rate-limit');
 
 const MongoClient = require('mongodb').MongoClient;
@@ -21,12 +21,13 @@ const auditLog = require('audit-log'); // DECLARE AUDITLOG
 const app = express();
 //----SSL-----------------------
 const https = require('https');
-const http = require('http');
+const http = require('http');//testmiljö
 const fs = require("fs");
 //---------------------------------
 
 
 //AUDIT LOGGING -----------------------
+//audit loggar i mongoDB, loggar user aktivitet 
 auditLog.addTransport("mongoose", {connectionString: "mongodb://localhost/auditdb"})
 //---------------------------------------
 
@@ -35,6 +36,7 @@ const uri = process.env.MONGODB;
 
 
 //SSL -private key and certifiering & Asymmetric encryption----------
+//Certificate , hemliga nycklar till SSL/TSL, som tillåter hemlig kommunikation mellan server och browser
 const options = {
     key: fs.readFileSync('nattas-key.pem'),
     cert: fs.readFileSync('nattas-cert.pem')
@@ -57,10 +59,10 @@ app.use(session({
     //cookie: { secure: true }
 }));
 
-app.use(passport.initialize()); //starting passport encryption
-app.use(passport.session());    //passport starting session cookies
+app.use(passport.initialize()); //starting passport encryption - passport startar kryptering
+app.use(passport.session());    //passport starting session cookies - passport startar session cookies
 
-//connection to our Mono DB where we have a document for our users.
+//connection to our Mongo DB where we have a document for our users.
 //if user registers with Google we can only see the Google ID and submitted Secret
 //if user registers via the form, we can see username, encrypted password and secret   
 mongoose.connect('mongodb://localhost:27017/userDB', {useNewUrlParser: true}); //Skapar connection till MongoDB med namnet userDB -Se i Compass
@@ -68,13 +70,17 @@ mongoose.connect('mongodb://localhost:27017/userDB', {useNewUrlParser: true}); /
 
 const userSchema = new mongoose.Schema ({
     email: String,
-    password: String
-    //googleId: String,
-    //secret: String
+    password: String,
+    googleId: String, //id som hittar user som loggar in genom google, och inte skapar ny id varje gång i DB
+    secret: String
 }); //Skapar en ny userSchema i mongoDB och Sparar email & password i mongoDB 
 
+//krypterar när man (call save plugin) , saltar och hashar automatiskt åt oss
 userSchema.plugin(passportLocalMongoose); // call plugin save = crypting
-// userSchema.plugin(findOrCreate);          // call plugin find = decrypting
+
+ //dekrypterar (när man call find plugin) , hittar avändare genom google id eller skapar om user inte finns
+userSchema.plugin(findOrCreate);          // call plugin find = decrypting
+
 
 //UTKOMMENTERAT FÖR ATT ISTÄLLET ANVÄNDA HASHING SOM ÄR SÄKRARE KRYPTERING
 //userSchema.plugin(encrypt, { secret: process.env.SECRET, encryptedFields: ["password"] }); 
@@ -82,15 +88,19 @@ userSchema.plugin(passportLocalMongoose); // call plugin save = crypting
 //Lägger till encryptedFields for att kategorisera ut och endast kryptera "password" -i detta fall.
 
 
-const User = new mongoose.model('User', userSchema); //Skapar User för userSchema
+const User = new mongoose.model('User', userSchema); //Skapar User för userSchema i mongoDB
+
 
 // PASSPORT------------------------------------
-    passport.use(User.createStrategy());
+//Autentisering
+passport.use(User.createStrategy());
 
+//serialize skapar  krypterad cookie med personens id, funkar för alla strategies
 // skapar cookies with user ID
 passport.serializeUser(function(user, done){
     done(null, user.id)
-});
+}); 
+//deserialize tar sönder cookie så vi kan identifiera personen
 // "opens" the cookie to identify user by ID
 passport.deserializeUser(function (id, done) {
     User.findById(id, function(err, user){
@@ -98,21 +108,24 @@ passport.deserializeUser(function (id, done) {
     });
 });
 
-// passport.use(new GoogleStrategy({
-//     clientID: process.env.CLIENT_ID,
-//     clientSecret: process.env.CLIENT_SECRET,
-//     callbackURL: "http://localhost:3000/auth/google/secrets",
-//     userProfileURL: "https://www.googleapis.com/oauth2/v3/userinfo"
-//   },
+//passport google oauth, autentiserar user med new google strategy
+passport.use(new GoogleStrategy({
+    clientID: process.env.CLIENT_ID,
+    clientSecret: process.env.CLIENT_SECRET,
+    callbackURL: "http://localhost:3000/auth/google/secrets", //URL från Google Developers
+    userProfileURL: "https://www.googleapis.com/oauth2/v3/userinfo" //Instead of retrieving the info from the users google+-account, instead we're retrieving from the user-info 
+  },                //hämtar info från userinfo inte google+ account -futureproofed
 
-//   function(accessToken, refreshToken, profile, cb) {
-//       console.log(profile)
+    //Auktorisering , google skickar accesstoken så att vi kan använda data så länge det behövs
+  function(accessToken, refreshToken, profile, cb) {
+      console.log(profile)
 
-//     User.findOrCreate({ googleId: profile.id }, function (err, user) {
-//       return cb(err, user);
-//     });
-//   }
-// ));
+        //installera paket mongoose findOrCreate, skapar el hittar id och profil genom googleid
+    User.findOrCreate({ googleId: profile.id }, function (err, user) {
+      return cb(err, user);
+    });
+  }
+));
 
 
 /* ----------------------------------------
@@ -126,7 +139,7 @@ const createLimiter = rateLimit({
 	message: "Too many accounts created from this IP, please try again after 15 minutes",
     standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
 	legacyHeaders: false, // Disable the `X-RateLimit-*` headers
-}) //Lägger till en login-spärr där man endast kan logga in 10ggr/15min
+}) //Lägger till en request-spärr där man endast kan requesta sidan 10ggr/15min
 const limiter = rateLimit({
 	windowMs: 15 * 60 * 1000, // 15 minutes
 	max: 10, // Limit each IP to 10 requests per `window` (we have set a window to be =  15 minutes)
@@ -137,50 +150,53 @@ const limiter = rateLimit({
 
 
 //**HOME ROUTE 
-app.get('/', limiter, function(req, res){
-    res.render('home') //Skickar användare till home som standard, utgår ifrån Home
+app.get("/", limiter, function(req, res){
+    res.render("home") //Skickar användare till home som standard, utgår ifrån Home
 });
 
 //**PASSPORT AND GOOGLE ROUTE-------------------
-//opens upp window for using google-sign in
-// app.get('/auth/google',
-//     passport.authenticate('google', { scope: ['profile'] })
-// );
+//opens up window for using google-sign in
+//passport auth med google strategy, popup ruta för att user ska logga in
+app.get("/auth/google", //getting info from google-server | Asking google server for the users profile
+    passport.authenticate('google', { scope: ["profile"] })
+); //Denna route skickar användaren vidare till google-inloggning
 
-// app.get('/auth/google/secrets', 
-//   passport.authenticate('google', { failureRedirect: '/login' }),
-//   function(req, res) {
-//     // Successful authentication, redirect to secrets-page.
-//     res.redirect('/secrets');
-//   });
+//google skickar tbx user, autentiserar user lokalt och blir hänvisad till appen för login
+app.get("/auth/google/secrets", 
+  passport.authenticate('google', { failureRedirect: "/login" }),
+  function(req, res) {
+    // Successful authentication, redirect to secrets-page.
+    res.redirect("/secrets");
+  });
 
 
 //**LOGIN ROUTE ----------------------
-app.get('/login', limiter, function(req, res){
-    res.render('login') //Skickar användaren till Login
+app.get("/login", limiter, function(req, res){
+    res.render("login") //Skickar användaren till Login
 });
 
 //**TERMS ROUTE ----------------------
-app.get('/terms', function(req, res){
-    res.render('terms') //Skickar användaren till Terms
+app.get("/terms", function(req, res){
+    res.render("/terms") //Skickar användaren till Terms
 });
 
 
 //REGISTRATION ROUTES ----------------------
-app.get('/register', limiter, function(req, res){
-    res.render('register') //Skickar användaren till Register
+app.get("/register", limiter, function(req, res){
+    res.render("register") //Skickar användaren till Register
 });
 
 //**SECRETS ROUTES ----------------------------
-app.get('/secrets', function (req, res){
+app.get("/secrets", function (req, res){
     if (req.isAuthenticated()){
-        res.render('secrets');
+        res.render("secrets");
     } else {
-        res.redirect('/login');
+        res.redirect("/login");
     }
 });
 
 //----ANOTHER SECRET ROUTE-------------------------
+//hittar alla secrets som blir inskickade
 // app.get('/secrets', function (req, res){
 //     //renders all submitted secrets 
 //     User.find({'secret': {$ne: null}}, function(err, foundUsers){
@@ -199,14 +215,20 @@ app.get('/secrets', function (req, res){
 //**SUBMIT SECRET ROUTE-------------------------------
 //if a user wants to access the /submit-page they need to be logged in 
 //- if not, they will be redirected to Log In
-// app.get('/submit', function (req,res) {
-//     if(req.isAuthenticated()){
-//         res.render('submit')
-//     }else {
-//         res.redirect('/login');
-//     }
-// });
+//skicka in en secret, om dom inte är inloggade skickas dom till login först
+app.get("/submit", function (req,res) {
+    if(req.isAuthenticated()){
+        res.render("submit")
+    }else {
+        res.redirect("/login");
+    }
+});
 
+
+//secret blir inskickad
+//när user blir aut, sparas info i req.user
+//om samma user loggar in igen så lagras deras secret på deras id
+//console.log(req.user.id);
 // app.post('/submit', function(req, res) {
 //     const submittedSecret = req.body.secret;
 
@@ -231,7 +253,8 @@ app.get('/secrets', function (req, res){
 //-----------------------------------------------------
 
 //**LOGOUT ROUTE------------------------------------------
-app.get('/logout', function(req, res, next) {
+//loggar ut användare avslutar session/cookies
+app.get("/logout", function(req, res, next) {
     // remove the req.user property and clear the login session
     req.logout();
   
@@ -249,15 +272,17 @@ If not, they remain on the same page
 
 //**REGISTER ROUTE NEW USER */
 
-app.post('/register', function(req, res){
+//om login lyckas kommer user in till secrets , annars blir user kvar på register
+//registrerar nya anändare med passport med local strategy
+app.post("/register", function(req, res){
   
     User.register({username: req.body.username}, req.body.password, function(err, user){
         if(err){
             console.log(err);
-            res.redirect('/register');
+            res.redirect("/register");
         } else {
-            passport.authenticate('local')(req, res, function(){
-                res.redirect('/secrets');
+            passport.authenticate("local")(req, res, function(){
+                res.redirect("/secrets");
             });
         }
     });
@@ -286,11 +311,14 @@ app.post('/register', function(req, res){
 //----------------------------------------------------------------
 
 //LOGIN ROUTES ----------------------
-app.post('/login', function(req, res){
+//kollar så att username och password matchar med passport
+app.post("/login", function(req, res){
     const user = new User({
         username: req.body.username,
         password: req.body.password
-    });
+    }); 
+
+    //auditlog funkade bara i denna function eftersom det är en post
     auditLog.logEvent(user.username, 'maybe script name or function',
     "what just happened", 'the affected target name perhaps', 'target id', 'additional info, JSON, etc.');
     
@@ -298,10 +326,10 @@ app.post('/login', function(req, res){
     req.login(user, function(err){
         if(err){
             console.log(err);
-            res.redirect('/login');
+            res.redirect("/login");
         } else{
-            passport.authenticate('local')(req, res, function(){
-                res.redirect('/secrets');
+            passport.authenticate("local")(req, res, function(){
+                res.redirect("/secrets");
             });
         }
     });
@@ -337,7 +365,8 @@ app.post('/login', function(req, res){
 //-------------------------------------------------------
 
 
-// friendly recaptcha
+//Accesslogging/Touring Test, recaptcha ser till att du inte är en robot
+//räknar även antal försök till login
 function recaptcha_callback() {
     var loginBtn = document.querySelector('#login-btn');
     loginBtn.removeAttribute('disabled');
